5484b161fec4f62a8cf7e2dc4f7a8811425748a8
Raquel Ramos Backes <raquel32132@hotmail.com>
2024-08-19 20:58:30 -0300

    API node com postgres

---
+++ server.js
@@ -0,0 +1,55 @@
+import { fastify } from 'fastify'
+//import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+//const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, reply) => {
+  const { title, description, duration } = request.body
+
+  await database.create({
+    title,
+    description,
+    duration,
+  })
+
+  console.log(database.list())
+
+  return reply.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+  const search = request.query.search
+
+  const videos = await database.list(search)
+
+  return videos
+})
+
+server.put('/videos/:id', async (request, reply) => {
+  const videoID = request.params.id
+  const { title, description, duration } = request.body
+
+  await database.update(videoID, {
+    title, 
+    description,
+    duration
+  })
+
+  return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+  const videoID = request.params.id
+
+  await database.delete(videoID)
+
+  return reply.status(204).send()
+})
+
+server.listen({
+  port: process.env.PORT ?? 3333,
+})
